97c5150aecae8bb3aa80a2400b7e646da5745d0e
emptynonsens <emptynonsens@gmail.com>
2022-07-31 21:23:56 +0200

    .

--- EasyOCR_Streamlit.py
+++ EasyOCR_Streamlit.py
@@ -7,28 +7,29 @@ import easyocr as ocr # optical character recognition
 
 loaded_image = st.file_uploader(label = "Upload your image here",type=['png','jpg','jpeg'])
 
-# Getting the model
-def ocr_model():
-    model = ocr.Reader(['en'],model_storage_directory='.', detector = True, recognizer = True)
-    return model
-
-# Preparing imgae to model's format
-def prepared_image(image = loaded_image):
-    img = Image.open(image)
-    #st.image(img)
-    img_arr = np.array(img)
-    return img_arr
-
-# applying model onto prepared image, returns appended lines of text
-def apply_model(model = ocr_model(), prepared_image = prepared_image()):
-    text_output = []
-    model_output = model.readtext(prepared_image)
-    for text in model_output:
-        st.write(text)
-        text_output.append(text[1]) # 1 = predictions https://www.jaided.ai/easyocr/
-    return text_output
-
 if loaded_image is not None:
+
+    # Getting the model
+    def ocr_model():
+        model = ocr.Reader(['en'],model_storage_directory='.', detector = True, recognizer = True)
+        return model
+
+    # Preparing imgae to model's format
+    def prepared_image(image = loaded_image):
+        img = Image.open(image)
+        #st.image(img)
+        img_arr = np.array(img)
+        return img_arr
+
+    # applying model onto prepared image, returns appended lines of text
+    def apply_model(model = ocr_model(), prepared_image = prepared_image()):
+        text_output = []
+        model_output = model.readtext(prepared_image)
+        for text in model_output:
+            st.write(text)
+            text_output.append(text[1]) # 1 = predictions https://www.jaided.ai/easyocr/
+        return text_output
+
     #input_image = Image.open(image)
     st.image(loaded_image)
     with st.spinner("computing"):
